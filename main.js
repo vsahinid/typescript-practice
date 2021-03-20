aba14480d377be35d9d029fa62e34b2186d4ad2b
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-20 13:54:48 -0400

    Added more

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var message = "Welcome back";
+console.log(message);
+var isBeginner = true;
+var total = 0;
+var name = "Vas";
+var sentence = "My name is " + name;
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var list1 = [1, 2, 3, 4];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Orange"] = 2] = "Orange";
+})(Color || (Color = {}));
+;
+var c = Color.Blue;
+var myVariable = 10;
+console.log(myVariable);
